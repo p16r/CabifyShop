6f46edf4db6eddd782d570ec4a7bdfdf7d85544e
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-01-31 12:51:59 +0530

    Add Product type

---
+++ CabifyShop/Models/Product.swift
@@ -0,0 +1,15 @@
+//
+//	Product.swift
+//	CabifyShop
+//
+//	Created by Prathamesh Kowarkar on 2023-01-31.
+//
+
+import Foundation
+
+struct Product: Hashable, Decodable {
+
+	let name: String
+	let price: Decimal
+
+}
