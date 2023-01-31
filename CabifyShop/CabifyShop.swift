4a78afe3af81b5ae76ad7ab94544273e809b8d89
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-01-31 12:09:12 +0530

    Initial commit

---
+++ CabifyShop/CabifyShop.swift
@@ -0,0 +1,19 @@
+//
+//	CabifyShop.swift
+//	CabifyShop
+//
+//	Created by Prathamesh Kowarkar on 2023-01-31.
+//
+
+import SwiftUI
+
+@main
+struct CabifyShop: App {
+
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+
+}
