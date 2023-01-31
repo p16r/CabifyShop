4a550688d3e03f0fa1c33837fa041ca88d8b3617
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-01-31 12:55:35 +0530

    Add sample catalog

--- CabifyShop/Models/Catalog.swift
+++ CabifyShop/Models/Catalog.swift
@@ -12,3 +12,34 @@ struct Catalog: Decodable {
 	let products: [Product]
 
 }
+
+extension Catalog {
+
+	static var sample: Self {
+		let json = """
+		{
+			"products": [
+				{
+					"code": "VOUCHER",
+					"name": "Cabify Voucher",
+					"price": 5
+				},
+				{
+					"code": "TSHIRT",
+					"name": "Cabify T-Shirt",
+					"price": 20
+				},
+				{
+					"code": "MUG",
+					"name": "Cabify Coffee Mug",
+					"price": 7.5
+				}
+			]
+		}
+		"""
+		let data = Data(json.utf8)
+		let catalog = try! JSONDecoder().decode(Self.self, from: data)
+		return catalog
+	}
+
+}
