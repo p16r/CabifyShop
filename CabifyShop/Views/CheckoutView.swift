1bca3346bed9a07282175a4920e5bd17122cab48
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-02-02 00:47:55 +0530

    Allow cart clearing in checkout view

--- CabifyShop/Views/CheckoutView.swift
+++ CabifyShop/Views/CheckoutView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct CheckoutView: View {
 
 	let cart: [Product]
-	let didPurchase: ([Product]) -> Void
+	let didCheckout: (CheckoutAction) -> Void
 
 	@Environment(\.dismiss) private var dismiss
 
 	@State private var isShowingConfirmPurchaseAlert = false
+	@State private var isShowingConfirmClearAlert = false
 
 	var body: some View {
 		ZStack(alignment: .bottom) {
@@ -45,21 +46,38 @@ struct CheckoutView: View {
 					}
 				}
 			}
-			Button(
-				action: {
-					isShowingConfirmPurchaseAlert = true
-				},
-				label: {
-					Image(systemName: "checkmark")
-					Text("Purchase")
-						.font(.headline)
-				}
-			)
-			.font(.headline)
-			.frame(maxWidth: .infinity, minHeight: 44)
-			.background(.blue)
-			.foregroundColor(.white)
-			.clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+			HStack {
+				Button(
+					action: {
+						isShowingConfirmPurchaseAlert = true
+					},
+					label: {
+						Image(systemName: "checkmark")
+						Text("Purchase")
+							.font(.headline)
+					}
+				)
+				.font(.headline)
+				.frame(maxWidth: .infinity, minHeight: 44)
+				.background(.blue)
+				.foregroundColor(.white)
+				.clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+				Button(
+					action: {
+						isShowingConfirmClearAlert = true
+					},
+					label: {
+						Image(systemName: "cart.fill.badge.minus")
+						Text("Clear cart")
+							.font(.headline)
+					}
+				)
+				.font(.headline)
+				.frame(maxWidth: .infinity, minHeight: 44)
+				.background(.red)
+				.foregroundColor(.white)
+				.clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+			}
 			.scenePadding(.horizontal)
 			.padding(.vertical, 16)
 			.background(.ultraThinMaterial)
@@ -69,7 +87,18 @@ struct CheckoutView: View {
 				title: Text("Confirm Purchase"),
 				message: Text("Proceed with purchase?"),
 				primaryButton: .default(Text("Purchase")) {
-					didPurchase(cart)
+					didCheckout(.purchased(cart))
+					dismiss()
+				},
+				secondaryButton: .cancel()
+			)
+		}
+		.alert(isPresented: $isShowingConfirmClearAlert) {
+			Alert(
+				title: Text("Confirm clear"),
+				message: Text("Clear cart?"),
+				primaryButton: .destructive(Text("Clear")) {
+					didCheckout(.cleared)
 					dismiss()
 				},
 				secondaryButton: .cancel()
