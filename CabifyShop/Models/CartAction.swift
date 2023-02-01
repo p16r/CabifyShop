8456303c94f2f55e9c6cfd7aa56f36dbdf130e69
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-02-01 17:11:53 +0530

    Add ProductDetailView

---
+++ CabifyShop/Models/CartAction.swift
@@ -0,0 +1,13 @@
+//
+//	CartAction.swift
+//	CabifyShop
+//
+//	Created by Prathamesh Kowarkar on 2023-02-01.
+//
+
+enum CartAction {
+
+	case added
+	case removed
+
+}
---
+++ CabifyShop/Views/ProductDetailView.swift
@@ -0,0 +1,58 @@
+//
+//	ProductDetailView.swift
+//	CabifyShop
+//
+//	Created by Prathamesh Kowarkar on 2023-01-31.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+
+	let product: Product
+	let cartAction: (CartAction, Product) -> Void
+
+	var body: some View {
+		VStack(alignment: .leading, spacing: 8) {
+			Text(product.code.icon)
+				.font(.system(size: 160))
+				.frame(maxWidth: .infinity)
+			Text(product.name)
+				.font(.title)
+			Text(product.price, format: .currency(code: "EUR"))
+				.font(.subheadline)
+				.padding(.bottom, 16)
+			HStack(spacing: 16) {
+				Button("Add To Cart") {
+					cartAction(.added, product)
+				}
+				.font(.body.bold())
+				.frame(maxWidth: .infinity, minHeight: 44)
+				.background(.blue)
+				.foregroundColor(.white)
+				.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+				Button("Remove from Cart") {
+					cartAction(.removed, product)
+				}
+				.frame(maxWidth: .infinity, minHeight: 44)
+				.background(.red)
+				.foregroundColor(.white)
+				.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+			}
+			.frame(maxWidth: .infinity)
+			Spacer()
+		}
+		.frame(maxWidth: .infinity)
+		.padding(.vertical, 16)
+		.scenePadding(.horizontal)
+	}
+
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+
+	static var previews: some View {
+		ProductDetailView(product: Catalog.sample.products[0]) { _, _ in }
+	}
+
+}
