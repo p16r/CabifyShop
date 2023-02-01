bc8f2b2908804ee3a076908c4f663ef3adb81868
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-02-02 00:45:36 +0530

    Add CheckoutAction

---
+++ CabifyShop/Models/CheckoutAction.swift
@@ -0,0 +1,15 @@
+//
+//	CheckoutAction.swift
+//	CabifyShop
+//
+//	Created by Prathamesh Kowarkar on 2023-02-02.
+//
+
+import Foundation
+
+enum CheckoutAction {
+
+	case purchased([Product])
+	case cleared
+
+}
