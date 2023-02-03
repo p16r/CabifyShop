edddc5419e3cd4906ba4a0d289566d6c8d5dd292
Prathamesh Kowarkar <hello@prathamesh.xyz>
2023-02-03 19:40:18 +0530

    Add test case setup and teardown code

--- CabifyShopTests/CabifyShopTests.swift
+++ CabifyShopTests/CabifyShopTests.swift
@@ -5,32 +5,33 @@
 //	Created by Prathamesh Kowarkar on 2023-01-31.
 //
 
+import Combine
 import XCTest
 @testable import CabifyShop
 
 final class CabifyShopTests: XCTestCase {
 
-	override func setUpWithError() throws {
-		// Put setup code here. This method is called before the invocation of each test method in the class.
+	private var viewModel: CatalogViewModel!
+	private var catalogAccumulator: Subscribers.Accumulate<Catalog>!
+	private var cartAccumulator: Subscribers.Accumulate<[CartItem]>!
+
+	override func setUp() {
+		super.setUp()
+		self.viewModel = CatalogViewModel(apiService: .mock())
+		self.catalogAccumulator = viewModel.$catalog
+			.dropFirst()
+			.compactMap(\.value)
+			.accumulate()
+		self.cartAccumulator = viewModel.$cart
+			.dropFirst()
+			.accumulate()
 	}
 
-	override func tearDownWithError() throws {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
-	}
-
-	func testExample() throws {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
-		// Any test you write for XCTest can be annotated as throws and async.
-		// Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-		// Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-	}
-
-	func testPerformanceExample() throws {
-		// This is an example of a performance test case.
-		self.measure {
-			// Put the code you want to measure the time of here.
-		}
+	override func tearDown() {
+		self.viewModel = nil
+		self.catalogAccumulator = nil
+		self.cartAccumulator = nil
+		super.tearDown()
 	}
 
 }
